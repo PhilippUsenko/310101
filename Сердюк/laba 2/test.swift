88b1457941e0f5fee64c5eb567f1b98b492c4c97
Ульяна <ulyana.siardziuk@gmail.com>
2024-12-05 12:42:18 +0300

    feat: create swift file

---
+++ Сердюк/laba 2/test.swift
@@ -0,0 +1,107 @@
+
+func isPrime(_ num: Int) -> Bool {
+    guard num > 1 else { return false }
+    if num == 2 { return true }
+    if num % 2 == 0 { return false }
+    let maxDivisor = Int(Double(num).squareRoot())
+    for i in 3...maxDivisor where i % 2 != 0 {
+        if num % i == 0 {
+            return false
+        }
+    }
+    return true
+}
+
+// MARK: - Задание 1: Числа Мерсенна
+
+func findMersenneNumbers(lessThan n: Int) -> [Int] {
+    var mersenneNumbers: [Int] = []
+    var p = 2
+    while true {
+        let mersenne = (1 << p) - 1  // 2^p - 1
+        if mersenne >= n {
+            break
+        }
+        if isPrime(p) && isPrime(mersenne) {
+            mersenneNumbers.append(mersenne)
+        }
+        p += 1
+    }
+    return mersenneNumbers
+}
+
+// MARK: - Задание 2: Обработка последовательности
+
+// Обрезка всех элементов после следующего четного числа
+func trimAfterNextEven(sequence: [Int]) -> [Int] {
+    guard !sequence.isEmpty, sequence.first! % 2 == 0 else { return sequence }
+    var result: [Int] = []
+    var evenFound = false
+    for number in sequence {
+        result.append(number)
+        if number % 2 == 0 {
+            if evenFound {
+                break
+            }
+            evenFound = true
+        }
+    }
+    return result
+}
+
+// Обрезка всех элементов, начиная с 4-го после первого нечетного числа
+func trimFromFourthAfterOdd(sequence: [Int]) -> [Int] {
+    guard !sequence.isEmpty, sequence.first! % 2 != 0 else { return sequence }
+    var countAfterOdd = 0
+    var indexToTrim: Int? = nil
+    
+    for (index, number) in sequence.enumerated() {
+        if index == 0 && number % 2 != 0 {
+            continue
+        }
+        if countAfterOdd >= 4 {
+            indexToTrim = index
+            break
+        }
+        countAfterOdd += 1
+    }
+    
+    if let trimIndex = indexToTrim {
+        return Array(sequence.prefix(trimIndex))
+    }
+    return sequence
+}
+
+// MARK: - Основная программа
+
+// Ввод значения n
+print("Введите натуральное число n:", terminator: " ")
+let n = Int(readLine()!) ?? 0
+
+if n <= 0 {
+    print("Ошибка: n должно быть натуральным числом.")
+} else {
+    // Задание 1: Найти числа Мерсенна
+    let mersenneNumbers = findMersenneNumbers(lessThan: n)
+    print("Числа Мерсенна меньше \(n): \(mersenneNumbers)")
+}
+
+// Ввод последовательности
+print("Введите последовательность чисел через пробел:", terminator: " ")
+let sequenceInput = readLine()!
+let sequence = sequenceInput.split(separator: " ").compactMap { Int($0) }
+
+if sequence.isEmpty {
+    print("Ошибка: последовательность не должна быть пустой и должна содержать только целые числа.")
+} else {
+    // Задание 2: Обработка последовательности
+    if let first = sequence.first {
+        if first % 2 == 0 {
+            let trimmed = trimAfterNextEven(sequence: sequence)
+            print("Обрезанная последовательность (после следующего четного): \(trimmed)")
+        } else {
+            let trimmed = trimFromFourthAfterOdd(sequence: sequence)
+            print("Обрезанная последовательность (с 4-го после нечетного): \(trimmed)")
+        }
+    }
+}
